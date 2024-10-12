7c798cafdfdd9906b8b2e244507b40653f1b05f8
Ropeth <121176515+Ropeth@users.noreply.github.com>
2024-10-12 14:37:01 +0100

    octave selector 1

--- src/components/MajMinSwitch.jsx
+++ src/components/MajMinSwitch.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 export default function MajMinSwitch({ setMajmin }) {
   const [up, setUp] = useState(+true);
   useEffect(() => {
--- src/components/OctaveSelector.jsx
+++ src/components/OctaveSelector.jsx
@@ -1,9 +1,15 @@
+import { useState, useEffect } from "react";
+
 export default function OctaveSelector({ setOctave }) {
+  const [oPos, setOpos] = useState(1);
+  useEffect(() => {
+    setOctave(oPos + 2);
+  }, [oPos]);
   return (
     <>
       <button
         onClick={() => {
-          setOctave(5);
+          oPos < 4 ? setOpos(oPos + 1) : setOpos(1);
         }}
       >
         change octave
